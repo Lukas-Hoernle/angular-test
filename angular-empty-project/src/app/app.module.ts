33891582371bba0cbb3b277b266061ef09aa71c8
Lukas.Hoernle <Lukas.Hoernle@cas.de>
2024-04-05 11:23:26 +0200

    separate components

--- angular-empty-project/src/app/app.module.ts
+++ angular-empty-project/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TemplateformComponent } from './templateform/templateform.component';
+import { ReactiveformComponent } from './reactiveform/reactiveform.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TemplateformComponent, ReactiveformComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
