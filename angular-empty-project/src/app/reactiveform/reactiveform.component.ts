0c955f117d938e60ce15136857fed17635471463
Lukas.Hoernle <Lukas.Hoernle@cas.de>
2024-04-05 12:56:46 +0200

    add reactiveform

--- angular-empty-project/src/app/reactiveform/reactiveform.component.ts
+++ angular-empty-project/src/app/reactiveform/reactiveform.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms'; 
 
 @Component({
   selector: 'app-reactiveform',
   templateUrl: './reactiveform.component.html',
-  styleUrls: ['./reactiveform.component.css']
+  styleUrls: ['./reactiveform.component.css'],
 })
 export class ReactiveformComponent {
+  reactiveForm: FormGroup; 
 
+  constructor(private formBuilder: FormBuilder) {
+    this.reactiveForm = this.formBuilder.group({
+      firstName: '', 
+      lastName: '', 
+    });
+  }
+
+  setFirstNameReactive() {
+    const firstName = this.reactiveForm.get('firstName').value;
+    const lastName = this.reactiveForm.get('lastName').value;
+    this.outputTemplate = `First Name: ${firstName} Last Name: ${lastName}`;
+  }
+
+  clearFirstNameReactive() {
+    this.reactiveForm.reset();
+    this.outputTemplate = ''; 
+  }
 }
